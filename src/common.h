168aa3a93aae605007a9e961ef6814ee7dc3b175
Furushchev <furushchev@mail.ru>
2014-02-14 20:18:19 +0900

    add common header

---
+++ src/common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <memory>
+
+#include <vector>
+#include <map>
+#include <array>
+#include <bitset>
+#include <valarray>
+#include <set>
+
+#include <cstdint>
+#include <cmath>
+#include <ratio>
+
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
