1031659256dac2b3926047fb2becf6e1e7ad9c9e
Furushchev <furushchev@mail.ru>
2014-02-14 20:45:20 +0900

    rm debug code

--- src/1.cpp
+++ src/1.cpp
@@ -6,14 +6,5 @@ auto main(int argc, char **argv) -> int {
     vector<int> v;
     for (int i = 1; i < 1000; ++i)
         if(i % 3 == 0 || i % 5 == 0) v.push_back(i);
-    //for (auto e : v) cout << e << endl;
     cout << accumulate(begin(v), end(v), 0, [](int x, int y){return x+y;}) << endl;
 }
-
-
-
-
-
-
-
-
